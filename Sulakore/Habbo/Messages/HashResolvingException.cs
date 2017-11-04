1a66ff371bb208f82f8c0a711919d6cb58683cff
ArachisH <iKBenPeanut@gmail.com>
2017-11-04 13:28:10 -0700

    Yes

--- Sulakore/Habbo/Messages/HashResolvingException.cs
+++ Sulakore/Habbo/Messages/HashResolvingException.cs
@@ -1,19 +1,30 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace Sulakore.Habbo.Messages
 {
     public sealed class HashResolvingException : Exception
     {
         public string Revision { get; }
-        public Dictionary<string, string[]> Unresolved { get; }
+        public ReadOnlyDictionary<string, string[]> Unresolved { get; }
 
+        public HashResolvingException(string revision, IList<string> hashes)
+            : base($"Failed to resolve '{hashes.Count}' hash value(s) from revision '{revision}'.")
+        {
+            Revision = revision;
+
+            Unresolved = new ReadOnlyDictionary<string, string[]>(
+                hashes.ToDictionary(u => u, u => new string[0]));
+        }
         public HashResolvingException(string revision, IDictionary<string, IList<string>> unresolved)
             : base($"Failed to resolve '{unresolved.Count}' hash value(s) from revision '{revision}'.")
         {
-            Unresolved = unresolved
-                .ToDictionary(u => u.Key, u => u.Value.ToArray());
+            Revision = revision;
+
+            Unresolved = new ReadOnlyDictionary<string, string[]>(
+                unresolved.ToDictionary(u => u.Key, u => u.Value.ToArray()));
         }
     }
 }

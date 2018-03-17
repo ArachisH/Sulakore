87391ee54c2352324f8235833eebf8a85b3b4f29
ArachisH <iKBenPeanut@gmail.com>
2018-03-17 15:38:41 -0700

    I changed things here, yea

--- Sulakore/Habbo/Web/HAPI.cs
+++ Sulakore/Habbo/Web/HAPI.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+
 using Sulakore.Network;
 
 namespace Sulakore.Habbo.Web
@@ -29,48 +30,96 @@ namespace Sulakore.Habbo.Web
             _client.DefaultRequestHeaders.Add("Cookie", "YPF8827340282Jdskjhfiw_928937459182JAX666=127.0.0.1");
         }
 
-        public static Task<HProfile> GetProfileAsync(string uniqueId) =>
-            GetContentAsync<HProfile>(HotelEndPoint.GetHotel(uniqueId), $"/api/public/users/{uniqueId}/profile");
-
-        public static Task<HUser> GetUserAsync(string name, HHotel hotel) =>
-            GetContentAsync<HUser>(hotel, ("/api/public/users?name=" + name));
-
-        public static Task<byte[]> GetFigureDataAsync(string figureId, char size) =>
-            GetContentAsync<byte[]>(HHotel.Com, $"/habbo-imaging/avatarimage?size={size}&figure={figureId}");
-
-        private static async Task<T> GetContentAsync<T>(HHotel hotel, string path)
+        public static async Task<string> GetLatestRevisionAsync(HHotel hotel)
         {
-            var address = new Uri($"https://www.habbo.{hotel.ToDomain()}{path}");
-            using (var request = new HttpRequestMessage(HttpMethod.Get, address))
-            using (HttpResponseMessage response = await _client.SendAsync(request).ConfigureAwait(false))
+            string body = await ReadContentAsync<string>(hotel.ToUri(), "/gamedata/external_variables/1").ConfigureAwait(false);
+            int revisionStartIndex = (body.LastIndexOf("/gordon/") + 8);
+            if (revisionStartIndex != 7)
             {
-                ServicePointManager.FindServicePoint(address).ConnectionLeaseTimeout = (30 * 1000);
-                if (!response.IsSuccessStatusCode) return default(T);
-
-                object content = null;
-                Type genericType = typeof(T);
-                switch (Type.GetTypeCode(genericType))
+                int revisionEndIndex = body.IndexOf('/', revisionStartIndex);
+                if (revisionEndIndex != -1)
                 {
-                    case TypeCode.String:
-                    content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-                    break;
-
-                    case TypeCode.Object:
-                    if (genericType == typeof(byte[]))
-                    {
-                        content = await response.Content.ReadAsByteArrayAsync().ConfigureAwait(false);
-                    }
-                    else if (genericType == typeof(HUser))
-                    {
-                        content = HUser.Create(await response.Content.ReadAsStringAsync().ConfigureAwait(false));
-                    }
-                    else if (genericType == typeof(HProfile))
-                    {
-                        content = HProfile.Create(await response.Content.ReadAsStringAsync().ConfigureAwait(false));
-                    }
-                    break;
+                    return body.Substring(revisionStartIndex, revisionEndIndex - revisionStartIndex);
                 }
-                return (T)content;
+            }
+            return null;
+        }
+        public static async Task<HProfile> GetProfileAsync(string name, HHotel hotel)
+        {
+            HUser user = await GetUserAsync(name, hotel).ConfigureAwait(false);
+            if (user.IsProfileVisible == true)
+            {
+                return await GetProfileAsync(user.UniqueId).ConfigureAwait(false);
+            }
+            return new HProfile { User = user };
+        }
+
+        public static Task<HGame> GetGameAsync(string revision)
+        {
+            return ReadContentAsync(HHotel.Com.ToUri("images"), $"/gordon/{revision}/Habbo.swf", async content =>
+            {
+                var game = new HGame(await content.ReadAsStreamAsync().ConfigureAwait(false));
+                game.Disassemble();
+                return game;
+            });
+        }
+        public static Task<string> DownloadGameAsync(string revision, string directoryName)
+        {
+            return ReadContentAsync(HHotel.Com.ToUri("images"), $"/gordon/{revision}/Habbo.swf", async content =>
+            {
+                string gamePath = Path.Combine(directoryName, "gordon", revision, "Habbo.swf");
+                Directory.CreateDirectory(Path.GetDirectoryName(gamePath));
+
+                using (Stream contentStream = await content.ReadAsStreamAsync().ConfigureAwait(false))
+                using (var fileStream = File.Create(gamePath))
+                {
+                    await contentStream.CopyToAsync(fileStream).ConfigureAwait(false);
+                }
+                return gamePath;
+            });
+        }
+
+        public static async Task<HGame> GetLatestGameAsync(HHotel hotel)
+        {
+            string latestRevision = await GetLatestRevisionAsync(hotel).ConfigureAwait(false);
+            return await GetGameAsync(latestRevision).ConfigureAwait(false);
+        }
+        public static async Task<string> DownloadLatestGameAsync(HHotel hotel, string path)
+        {
+            string latestRevision = await GetLatestRevisionAsync(hotel).ConfigureAwait(false);
+            return await DownloadGameAsync(latestRevision, path).ConfigureAwait(false);
+        }
+
+        public static Task<byte[]> GetFigureDataAsync(string query) =>
+            ReadContentAsync<byte[]>(HHotel.Com.ToUri(), ("/habbo-imaging/avatarimage?" + query));
+
+        public static async Task<HUser> GetUserAsync(string name, HHotel hotel) =>
+            HUser.Create(await ReadContentAsync<string>(hotel.ToUri(), ("/api/public/users?name=" + name)));
+
+        public static async Task<HProfile> GetProfileAsync(string uniqueId) =>
+            HProfile.Create(await ReadContentAsync<string>(HotelEndPoint.GetHotel(uniqueId).ToUri(), $"/api/public/users/{uniqueId}/profile"));
+
+        public static async Task<T> ReadContentAsync<T>(Uri baseUri, string path, Func<HttpContent, Task<T>> contentConverter = null)
+        {
+            using (var request = new HttpRequestMessage(HttpMethod.Get, $"{baseUri.GetLeftPart(UriPartial.Authority)}{path}"))
+            using (HttpResponseMessage response = await _client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead).ConfigureAwait(false))
+            {
+                ServicePointManager.FindServicePoint(request.RequestUri).ConnectionLeaseTimeout = (30 * 1000);
+                if (!response.IsSuccessStatusCode) return default(T);
+                if (contentConverter == null)
+                {
+                    Type genericType = typeof(T);
+                    if (genericType == typeof(string))
+                    {
+                        return (T)(object)await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                    }
+                    else if (genericType == typeof(byte[]))
+                    {
+                        return (T)(object)await response.Content.ReadAsByteArrayAsync().ConfigureAwait(false);
+                    }
+                    else return default(T);
+                }
+                return await contentConverter(response.Content).ConfigureAwait(false);
             }
         }
     }

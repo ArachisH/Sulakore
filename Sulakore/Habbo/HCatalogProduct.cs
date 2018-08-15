c2e83242114cefca83e46dbbfbdded0ed4cbff27
Speaqer <speaqerpr0@gmail.com>
2018-08-15 20:21:35 +0300

    Added various catalog related parsers.

---
+++ Sulakore/Habbo/HCatalogProduct.cs
@@ -0,0 +1,43 @@
+﻿using Sulakore.Network.Protocol;
+
+namespace Sulakore.Habbo
+{
+    public class HCatalogProduct
+    {
+        public HProductType Type { get; set; }
+        public int ClassId { get; set; }
+
+        public string Extradata { get; set; }
+        public int ProductCount { get; set; }
+
+        public bool IsLimited { get; set; }
+        public int LimitedTotal { get; set; }
+        public int LimitedRemaining { get; set; }
+
+        public HCatalogProduct(HPacket packet)
+        {
+            Type = (HProductType)packet.ReadUTF8()[0];
+            switch (Type)
+            {
+                case HProductType.Badge:
+                {
+                    Extradata = packet.ReadUTF8();
+                    ProductCount = 1;
+                    break;
+                }
+                default:
+                {
+                    ClassId = packet.ReadInt32();
+                    Extradata = packet.ReadUTF8();
+                    ProductCount = packet.ReadInt32();
+                    if(IsLimited = packet.ReadBoolean())
+                    {
+                        LimitedTotal = packet.ReadInt32();
+                        LimitedRemaining = packet.ReadInt32();
+                    } 
+                    break;
+                }
+            }
+        }
+    }
+}

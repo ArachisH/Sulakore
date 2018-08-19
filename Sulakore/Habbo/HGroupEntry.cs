9f5c29c00b415dbe48ca6c848a13d21f6526ced9
Speaqer <speaqerpr0@gmail.com>
2018-08-19 21:13:24 +0300

    Add models for user profile reading and writing

---
+++ Sulakore/Habbo/HGroupEntry.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using Sulakore.Network.Protocol;
+
+namespace Sulakore.Habbo
+{
+    public class HGroupEntry : IHabboData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string BadgeCode { get; set; }
+        public string PrimaryColor { get; set; }
+        public string SecondaryColor { get; set; }
+
+        public bool Favorite { get; set; }
+        public int OwnerId { get; set; }
+        public bool HasForum { get; set; }
+
+        public HGroupEntry(HPacket packet)
+        {
+            Id = packet.ReadInt32();
+            Name = packet.ReadUTF8();
+            BadgeCode = packet.ReadUTF8();
+            PrimaryColor = packet.ReadUTF8();
+            SecondaryColor = packet.ReadUTF8();
+
+            Favorite = packet.ReadBoolean();
+            OwnerId = packet.ReadInt32();
+            HasForum = packet.ReadBoolean();
+        }
+
+        public void WriteTo(HPacket packet)
+        {
+            packet.Write(Id);
+            packet.Write(Name);
+            packet.Write(BadgeCode);
+            packet.Write(PrimaryColor);
+            packet.Write(SecondaryColor);
+
+            packet.Write(Favorite);
+            packet.Write(OwnerId);
+            packet.Write(HasForum);
+        }
+        
+        public byte[] ToBytes() => throw new NotImplementedException();
+        public HPacket ToPacket() => throw new NotSupportedException();
+    }
+}
---
+++ Sulakore/Habbo/HUserProfile.cs
@@ -0,0 +1,71 @@
+﻿using Sulakore.Network.Protocol;
+
+namespace Sulakore.Habbo
+{
+    public class HUserProfile : HData
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Motto { get; set; }
+        public string Figure { get; set; }
+        public string CreationDate { get; set; }
+        public int AchievementScore { get; set; }
+        public int FriendCount { get; set; }
+
+        public bool IsFriend { get; set; }
+        public bool IsFriendRequestSent { get; set; }
+        public bool IsOnline { get; set; }
+
+        public HGroupEntry[] Groups { get; set; }
+
+        public int LastAccessSinceInSeconds { get; set; }
+        public bool OpenProfileView { get; set; }
+
+        public HUserProfile(HPacket packet)
+            : base(packet)
+        {
+            Id = packet.ReadInt32();
+            Username = packet.ReadUTF8();
+            Motto = packet.ReadUTF8();
+            Figure = packet.ReadUTF8();
+            CreationDate = packet.ReadUTF8();
+            AchievementScore = packet.ReadInt32();
+            FriendCount = packet.ReadInt32();
+
+            IsFriend = packet.ReadBoolean();
+            IsFriendRequestSent = packet.ReadBoolean();
+            IsOnline = packet.ReadBoolean();
+
+            Groups = new HGroupEntry[packet.ReadInt32()];
+            for (int i = 0; i < Groups.Length; i++)
+            {
+                Groups[i] = new HGroupEntry(packet);
+            }
+            LastAccessSinceInSeconds = packet.ReadInt32();
+            OpenProfileView = packet.ReadBoolean();
+        }
+
+        public override void WriteTo(HPacket packet)
+        {
+            packet.Write(Id);
+            packet.Write(Username);
+            packet.Write(Motto);
+            packet.Write(Figure);
+            packet.Write(CreationDate);
+            packet.Write(AchievementScore);
+            packet.Write(FriendCount);
+
+            packet.Write(IsFriend);
+            packet.Write(IsFriendRequestSent);
+            packet.Write(IsOnline);
+
+            packet.Write(Groups.Length);
+            foreach(var group in Groups)
+            {
+                group.WriteTo(packet);
+            }
+            packet.Write(LastAccessSinceInSeconds);
+            packet.Write(OpenProfileView);
+        }
+    }
+}

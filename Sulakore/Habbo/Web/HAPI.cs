22e976ba3a146f86b235617e3622eaeb516c1225
Speaqer <speaqerpr0@gmail.com>
2020-04-11 19:52:35 +0300

    Expose JsonSerializerOptions

--- Sulakore/Habbo/Web/HAPI.cs
+++ Sulakore/Habbo/Web/HAPI.cs
@@ -14,8 +14,9 @@ namespace Sulakore.Habbo.Web
     {
         private static readonly HttpClient _client;
         private static readonly HttpClientHandler _handler;
-        private static readonly JsonSerializerOptions _serializerOptions;
 
+        public static JsonSerializerOptions SerializerOptions { get; }
+        
         public const string CHROME_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36";
 
         static HAPI()
@@ -32,16 +33,16 @@ namespace Sulakore.Habbo.Web
             _client.DefaultRequestHeaders.Add("User-Agent", CHROME_USER_AGENT);
             _client.DefaultRequestHeaders.Add("Cookie", "YPF8827340282Jdskjhfiw_928937459182JAX666=127.0.0.1");
 
-            _serializerOptions = new JsonSerializerOptions
+            SerializerOptions = new JsonSerializerOptions
             {
                 PropertyNamingPolicy = JsonNamingPolicy.CamelCase
             };
-            _serializerOptions.Converters.Add(new DateTimeConverter());
+            SerializerOptions.Converters.Add(new DateTimeConverter());
         }
 
         public static Task<byte[]> GetFigureDataAsync(string query) => ReadContentAsync<byte[]>(HHotel.Com.ToUri(), "/habbo-imaging/avatarimage?" + query);
-        public static async Task<HUser> GetUserAsync(string name, HHotel hotel) => await ReadContentAsync<HUser>(hotel.ToUri(), "/api/public/users?name=" + name);
-        public static async Task<HProfile> GetProfileAsync(string uniqueId) => await ReadContentAsync<HProfile>(HotelEndPoint.GetHotel(uniqueId).ToUri(), $"/api/public/users/{uniqueId}/profile");
+        public static Task<HUser> GetUserAsync(string name, HHotel hotel) => ReadContentAsync<HUser>(hotel.ToUri(), "/api/public/users?name=" + name);
+        public static Task<HProfile> GetProfileAsync(string uniqueId) => ReadContentAsync<HProfile>(HotelEndPoint.GetHotel(uniqueId).ToUri(), $"/api/public/users/{uniqueId}/profile");
 
         public static async Task<string> GetLatestRevisionAsync(HHotel hotel)
         {
@@ -122,26 +123,26 @@ namespace Sulakore.Habbo.Web
         {
             using var request = new HttpRequestMessage(HttpMethod.Get, baseUri.GetLeftPart(UriPartial.Authority) + path);
             using HttpResponseMessage response = await _client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead).ConfigureAwait(false);
-            
+
             ServicePointManager.FindServicePoint(request.RequestUri).ConnectionLeaseTimeout = 30 * 1000;
-            
+
             if (!response.IsSuccessStatusCode) return default;
-            if (contentConverter == null)
-            {
-                Type genericType = typeof(T);
 
-                if (genericType == typeof(string))
-                    return (T)(object)await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            if (contentConverter != null)
+                return await contentConverter(response.Content).ConfigureAwait(false);
 
-                if (genericType == typeof(byte[]))
-                    return (T)(object)await response.Content.ReadAsByteArrayAsync().ConfigureAwait(false);
+            Type genericType = typeof(T);
 
-                if (response.Content.Headers.ContentType.MediaType == "application/json")
-                    return await JsonSerializer.DeserializeAsync<T>(await response.Content.ReadAsStreamAsync().ConfigureAwait(false), _serializerOptions).ConfigureAwait(false);
-                
-                return default;
-            }
-            return await contentConverter(response.Content).ConfigureAwait(false);
+            if (genericType == typeof(string))
+                return (T)(object)await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            if (genericType == typeof(byte[]))
+                return (T)(object)await response.Content.ReadAsByteArrayAsync().ConfigureAwait(false);
+
+            if (response.Content.Headers.ContentType.MediaType == "application/json")
+                return await JsonSerializer.DeserializeAsync<T>(await response.Content.ReadAsStreamAsync().ConfigureAwait(false), SerializerOptions).ConfigureAwait(false);
+
+            return default;
         }
     }
 }

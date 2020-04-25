cef16e9a59e5787efe58e5216a91beaddd9012a0
Speaqer <speaqerpr0@gmail.com>
2020-04-25 19:52:26 +0300

    Add Achievement badge parser

---
+++ Sulakore/Habbo/HAchievement.cs
@@ -0,0 +1,30 @@
+﻿using Sulakore.Network.Protocol;
+
+namespace Sulakore.Habbo
+{
+    public class HAchievement
+    {
+        public string Name { get; set; }
+        public HAchievementLevel[] Levels { get; set; }
+
+        public HAchievement(HPacket packet)
+        {
+            Name = packet.ReadUTF8();
+            Levels = new HAchievementLevel[packet.ReadInt32()];
+            for (int i = 0; i < Levels.Length; i++)
+            {
+                Levels[i] = new HAchievementLevel(Name, packet);
+            }
+        }
+
+        public static HAchievement[] Parse(HPacket packet)
+        {
+            var achievements = new HAchievement[packet.ReadInt32()];
+            for (int i = 0; i < achievements.Length; i++)
+            {
+                achievements[i] = new HAchievement(packet);
+            }
+            return achievements;
+        }
+    }
+}

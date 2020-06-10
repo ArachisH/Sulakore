becce0f43e4f2290a2bbdca3f11214da193dbe45
Speaqer <speaqerpr0@gmail.com>
2020-06-10 17:47:42 +0300

    Read bunch of HItem values properly

---
+++ Sulakore/Habbo/Enums/HFurniCategory.cs
@@ -0,0 +1,39 @@
+﻿namespace Sulakore.Habbo
+{
+    public enum HFurniCategory
+    {
+        Default = 1,
+
+        Wallpaper = 2,
+        Floor = 3,
+        Landscape = 4,
+
+        PostIt = 5,
+        Poster = 6,
+
+        SoundSet = 7,
+        TraxSong = 8,
+        
+        Present = 9,
+        EcotronBox = 10,
+        
+        Trophy = 11,
+        
+        CreditFurni = 12,
+
+        PetShampoo = 13,
+        PetCustomPart = 14,
+        PetCustomPartShampoo = 15,
+        PetSaddle = 16,
+
+        GuildFurni = 17,
+        GameFurni = 18,
+
+        MonsterplantSeed = 19,
+        MonsterplantRevival = 20,
+        MonsterplantRebreed = 21,
+        MonsterplantFertilize = 22,
+
+        FigurePurchasableSet = 23,
+    }
+}
--- Sulakore/Habbo/HItem.cs
+++ Sulakore/Habbo/HItem.cs
@@ -6,43 +6,56 @@ namespace Sulakore.Habbo
     public class HItem : HData
     {
         public int Id { get; set; }
+        public HProductType Type { get; set; }
         public int TypeId { get; set; }
-        public int RoomId { get; set; }
-        public int Category { get; set; }
-        public string? SlotId { get; set; }
+
+        public HFurniCategory Category { get; set; }
+
+        public int StuffDataFormat { get; set; } //TODO: This is temporary name, StuffData logic is under rewrite :)
         public object[] Stuff { get; set; }
-        public bool IsTradeable { get; set; }
+
+        public bool IsRecyclable { get; set; }
+        public bool IsTradable { get; set; }
         public bool IsGroupable { get; set; }
+        public bool IsSellable { get; set; }
+
         public int SecondsToExpiration { get; set; }
-        public bool HasRentPeriodStarted { get; set; }
         public bool CanPlaceInMarketplace { get; set; }
+
+        public bool HasRentPeriodStarted { get; set; }
+        public int RoomId { get; set; }
+        
+        public string? SlotId { get; set; }
+        public int? Extra { get; set; }
 
         public HItem(HPacket packet)
         {
             packet.ReadInt32();
 
-            string loc2 = packet.ReadUTF8();
+            Type = (HProductType)packet.ReadUTF8()[0];
 
             Id = packet.ReadInt32();
             TypeId = packet.ReadInt32();
-            packet.ReadInt32();
+            Category = (HFurniCategory)packet.ReadInt32();
 
-            Category = packet.ReadInt32();
-            Stuff = ReadData(packet, Category);
+            StuffDataFormat = packet.ReadInt32();
+            Stuff = ReadData(packet, StuffDataFormat);
 
+            IsRecyclable =  packet.ReadBoolean();
+            IsTradable = packet.ReadBoolean();
             IsGroupable = packet.ReadBoolean();
-            IsTradeable = packet.ReadBoolean();
-            packet.ReadBoolean();
+            IsSellable = packet.ReadBoolean();
+
             CanPlaceInMarketplace = packet.ReadBoolean();
             SecondsToExpiration = packet.ReadInt32();
 
             HasRentPeriodStarted = packet.ReadBoolean();
             RoomId = packet.ReadInt32();
 
-            if (loc2 == "S")
+            if (Type == HProductType.Stuff)
             {
                 SlotId = packet.ReadUTF8();
-                packet.ReadInt32();
+                Extra = packet.ReadInt32();
             }
         }
 

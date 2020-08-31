2fe5af053bbdcc7a865c43c7be271f7fc1385169
xnumad <34810600+xnumad@users.noreply.github.com>
2020-08-31 23:48:47 +0200

    Add UpdateThread

--- Sulakore/Habbo/Messages/Outgoing.cs
+++ Sulakore/Habbo/Messages/Outgoing.cs
@@ -352,6 +352,7 @@ namespace Sulakore.Habbo.Messages
         public HMessage UpdateGuildColors { get; set; }
         public HMessage UpdateGuildIdentity { get; set; }
         public HMessage UpdateGuildSettings { get; set; }
+        public HMessage UpdateThread { get; set; }
         public HMessage UseFurniture { get; set; }
         public HMessage UseWallItem { get; set; }
         public HMessage UserSaveLook { get; set; }

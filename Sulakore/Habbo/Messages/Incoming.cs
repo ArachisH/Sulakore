b47680c96a604d9ea4a70be2f59ccab0dcae992c
xnumad <34810600+xnumad@users.noreply.github.com>
2020-08-31 10:55:18 +0200

    Add SeasonalCalendarDailyOffer

--- Sulakore/Habbo/Messages/Incoming.cs
+++ Sulakore/Habbo/Messages/Incoming.cs
@@ -356,6 +356,7 @@ namespace Sulakore.Habbo.Messages
         public HMessage RoomUserUnbanned { get; set; }
         public HMessage RoomVisualizationSettings { get; set; }
         public HMessage SanctionStatus { get; set; }
+        public HMessage SeasonalCalendarDailyOffer { get; set; }
         public HMessage SellablePetPalettes { get; set; }
         public HMessage Shout { get; set; }
         public HMessage ShowMysteryBoxWait { get; set; }

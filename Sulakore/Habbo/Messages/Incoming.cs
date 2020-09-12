c4f1c26d84b5fdf7117543038acd8fba429005ff
xnumad <34810600+xnumad@users.noreply.github.com>
2020-09-12 23:02:22 +0200

    SimplePollAnswer -> QuestionAnswered

--- Sulakore/Habbo/Messages/Incoming.cs
+++ Sulakore/Habbo/Messages/Incoming.cs
@@ -304,6 +304,7 @@ namespace Sulakore.Habbo.Messages
         public HMessage QuestCompleted { get; set; }
         public HMessage QuestExpired { get; set; }
         public HMessage Question { get; set; }
+        public HMessage QuestionAnswered { get; set; }
         public HMessage QuizData { get; set; }
         public HMessage QuizResults { get; set; }
         public HMessage RecyclerComplete { get; set; }
@@ -360,7 +361,6 @@ namespace Sulakore.Habbo.Messages
         public HMessage SellablePetPalettes { get; set; }
         public HMessage Shout { get; set; }
         public HMessage ShowMysteryBoxWait { get; set; }
-        public HMessage SimplePollAnswer { get; set; }
         public HMessage SimplePollAnswers { get; set; }
         public HMessage Sleep { get; set; }
         public HMessage SlideObjectBundle { get; set; }

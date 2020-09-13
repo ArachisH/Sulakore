e3eb9bdd3075612decb02a163c19bdf24bd9ed30
xnumad <34810600+xnumad@users.noreply.github.com>
2020-09-13 14:47:20 +0200

    JukeBoxPlaylistFull -> JukeboxPlayListFull

--- Sulakore/Habbo/Messages/Incoming.cs
+++ Sulakore/Habbo/Messages/Incoming.cs
@@ -198,7 +198,7 @@ namespace Sulakore.Habbo.Messages
         public HMessage ItemRemove { get; set; }
         public HMessage ItemUpdate { get; set; }
         public HMessage Items { get; set; }
-        public HMessage JukeBoxPlaylistFull { get; set; }
+        public HMessage JukeboxPlayListFull { get; set; }
         public HMessage JukeboxSongDisks { get; set; }
         public HMessage LatencyPingResponse { get; set; }
         public HMessage LeprechaunStarterBundle { get; set; }

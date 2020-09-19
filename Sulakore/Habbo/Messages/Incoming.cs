4bed4d723d4b08fc81170b0d6d803909ce029f1e
xnumad <34810600+xnumad@users.noreply.github.com>
2020-09-19 10:35:24 +0200

    TradeCloseWindow -> TradingCompleted

--- Sulakore/Habbo/Messages/Incoming.cs
+++ Sulakore/Habbo/Messages/Incoming.cs
@@ -368,12 +368,12 @@ namespace Sulakore.Habbo.Messages
         public HMessage TargetedOffer { get; set; }
         public HMessage TargetedOfferNotFound { get; set; }
         public HMessage TradeAccepted { get; set; }
-        public HMessage TradeCloseWindow { get; set; }
         public HMessage TradeComplete { get; set; }
         public HMessage TradeOpenFailed { get; set; }
         public HMessage TradeStart { get; set; }
         public HMessage TradeStopped { get; set; }
         public HMessage TradeUpdate { get; set; }
+        public HMessage TradingCompleted { get; set; }
         public HMessage TradingWaitingConfirm { get; set; }
         public HMessage TraxSongInfo { get; set; }
         public HMessage UniqueMachineID { get; set; }

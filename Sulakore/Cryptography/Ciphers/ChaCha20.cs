f7e7858efc23658f65a83cb27e963a3baf53d08c
ArachisH <iKBenPeanut@gmail.com>
2020-12-17 19:10:44 -0800

    * Implement ChaCha20 state generation thing

--- Sulakore/Cryptography/Ciphers/ChaCha20.cs
+++ Sulakore/Cryptography/Ciphers/ChaCha20.cs
@@ -5,19 +5,21 @@ namespace Sulakore.Cryptography.Ciphers
 {
     public class ChaCha20 : IStreamCipher
     {
-        private static readonly int[] _constants = new int[4] { 0x61707865, 0x3320646e, 0x79622d32, 0x6b206574 };
+        private static readonly uint[] _constants = new uint[4] { 0x61707865, 0x3320646e, 0x79622d32, 0x6b206574 };
+
+        private readonly uint[] _originalState;
 
         private bool _disposed;
         private byte[] _table;
 
-        public ChaCha20(ReadOnlySpan<byte> key, ReadOnlySpan<byte> nonce, int blockCount = 0)
+        public ChaCha20(ReadOnlySpan<byte> key, ReadOnlySpan<byte> nonce, uint blockCount = 1)
         {
             if (key.Length != 32) throw new ArgumentException("The provided key is not 256-bits.");
             if (nonce.Length != 12) throw new ArgumentException("The provided nonce is not 96-bits.");
 
             _table = new byte[64];
             Span<byte> tableBytes = _table.AsSpan();
-            Span<int> tableIntegers = MemoryMarshal.Cast<byte, int>(tableBytes);
+            Span<uint> tableIntegers = MemoryMarshal.Cast<byte, uint>(tableBytes);
 
             _constants.CopyTo(tableIntegers);
             key.CopyTo(tableBytes.Slice(_constants.Length * sizeof(int)));
@@ -25,22 +27,34 @@ namespace Sulakore.Cryptography.Ciphers
             tableIntegers[12] = blockCount;
             nonce.CopyTo(tableBytes.Slice(13 * sizeof(int)));
 
-            QuarterRound(tableIntegers, 0, 0, 0, 0);
+            tableIntegers.CopyTo(_originalState);
+            for (int i = 0; i < 10; i++)
+            {
+                QuarterRound(tableIntegers, 0, 4, 8, 12);
+                QuarterRound(tableIntegers, 1, 5, 9, 13);
+                QuarterRound(tableIntegers, 2, 6, 10, 14);
+                QuarterRound(tableIntegers, 3, 7, 11, 15);
+                QuarterRound(tableIntegers, 0, 5, 10, 15);
+                QuarterRound(tableIntegers, 1, 6, 11, 12);
+                QuarterRound(tableIntegers, 2, 7, 8, 13);
+                QuarterRound(tableIntegers, 3, 4, 9, 14);
+            }
         }
 
-        public void Process(Span<byte> data)
-        {
-            throw new NotImplementedException();
-        }
+        public void Process(Span<byte> data) => Process(data, data);
         public void Process(ReadOnlySpan<byte> data, Span<byte> parsed)
         {
-            throw new NotImplementedException();
+            // TODO: https://tools.ietf.org/html/rfc7539#section-2.4.1
         }
 
-        private void QuarterRound(Span<int> state, uint a, uint b, uint c, uint d)
+        private static void QuarterRound(Span<uint> state, int a, int b, int c, int d)
         {
-
+            state[d] = RotateLeft(state[d] ^ (state[a] += state[b]), 16);
+            state[b] = RotateLeft(state[b] ^ (state[c] += state[d]), 12);
+            state[d] = RotateLeft(state[d] ^ (state[a] += state[b]), 8);
+            state[b] = RotateLeft(state[b] ^ (state[c] += state[d]), 7);
         }
+        private static uint RotateLeft(uint value, int count) => (value << count) | (value >> (32 - count));
 
         public void Dispose()
         {

a96991c662fbbab5dd049f074a0f0b889f72a5b3
xnumad <34810600+xnumad@users.noreply.github.com>
2020-12-20 21:49:56 +0100

    Add ModerateMessage

--- Sulakore/Habbo/Messages/Outgoing.cs
+++ Sulakore/Habbo/Messages/Outgoing.cs
@@ -225,6 +225,7 @@ namespace Sulakore.Habbo.Messages
         public HMessage ModToolSanctionBan { get; set; }
         public HMessage ModToolSanctionMute { get; set; }
         public HMessage ModToolSanctionTradeLock { get; set; }
+        public HMessage ModerateMessage { get; set; }
         public HMessage MoodLightSaveSettings { get; set; }
         public HMessage MoodLightSettings { get; set; }
         public HMessage MoodLightTurnOn { get; set; }

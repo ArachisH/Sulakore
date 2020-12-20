2f37383a338d3e4e4be8f8af99db9dfe51eb399f
xnumad <34810600+xnumad@users.noreply.github.com>
2020-12-20 21:24:01 +0100

    Add CancelPetBreeding

--- Sulakore/Habbo/Messages/Outgoing.cs
+++ Sulakore/Habbo/Messages/Outgoing.cs
@@ -33,6 +33,7 @@ namespace Sulakore.Habbo.Messages
         public HMessage CallForHelp { get; set; }
         public HMessage CallForHelpFromIM { get; set; }
         public HMessage CancelMarketplaceOffer { get; set; }
+        public HMessage CancelPetBreeding { get; set; }
         public HMessage CancelQuest { get; set; }
         public HMessage CancelTyping { get; set; }
         public HMessage ChangeEmail { get; set; }

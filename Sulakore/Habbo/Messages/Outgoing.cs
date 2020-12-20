c6a55d052a18ba8d3c4c7033b1f59db1392ccf56
xnumad <34810600+xnumad@users.noreply.github.com>
2020-12-20 21:58:39 +0100

    Add UpdateRoomThumbnail
    It’s the old room thumbnail where you combined predefined images

--- Sulakore/Habbo/Messages/Outgoing.cs
+++ Sulakore/Habbo/Messages/Outgoing.cs
@@ -437,6 +437,7 @@ namespace Sulakore.Habbo.Messages
         public HMessage UpdateGuildIdentity { get; set; }
         public HMessage UpdateGuildSettings { get; set; }
         public HMessage UpdateRoomCategoryAndTradeSettings { get; set; }
+        public HMessage UpdateRoomThumbnail { get; set; }
         public HMessage UpdateThread { get; set; }
         public HMessage UseFurniture { get; set; }
         public HMessage UseWallItem { get; set; }

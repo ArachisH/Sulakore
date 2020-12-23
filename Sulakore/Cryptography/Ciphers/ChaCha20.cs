dd9a1e75c8e7d0d22b385cbc5227dca0453ad52b
ArachisH <iKBenPeanut@gmail.com>
2020-12-23 13:22:39 -0800

    * Kind of works

--- Sulakore/Cryptography/Ciphers/ChaCha20.cs
+++ Sulakore/Cryptography/Ciphers/ChaCha20.cs
@@ -7,54 +7,99 @@ namespace Sulakore.Cryptography.Ciphers
     {
         private static readonly uint[] _constants = new uint[4] { 0x61707865, 0x3320646e, 0x79622d32, 0x6b206574 };
 
-        private readonly uint[] _originalState;
+        private readonly byte[] _state;
 
+        public bool IsDecrypting { get; set; }
+
+        private byte[] _block;
         private bool _disposed;
-        private byte[] _table;
+        private int _position = -1;
 
         public ChaCha20(ReadOnlySpan<byte> key, ReadOnlySpan<byte> nonce, uint blockCount = 0)
         {
             if (key.Length != 32) throw new ArgumentException("The provided key is not 256-bits.");
-            if (nonce.Length != 12) throw new ArgumentException("The provided nonce is not 96-bits.");
+            if (nonce.Length != 8 && nonce.Length != 12) throw new ArgumentException("The provided nonce can only be 96-bit or 64-bit.");
 
-            _table = new byte[64];
-            Span<byte> tableBytes = _table.AsSpan();
-            Span<uint> tableIntegers = MemoryMarshal.Cast<byte, uint>(tableBytes);
+            _state = new byte[64];
+            Span<uint> state = MemoryMarshal.Cast<byte, uint>(_state);
 
-            _constants.CopyTo(tableIntegers);
-            key.CopyTo(tableBytes.Slice(_constants.Length * sizeof(int)));
+            // 4 Unsigned Integers
+            _constants.CopyTo(state);
 
-            tableIntegers[12] = blockCount;
-            nonce.CopyTo(tableBytes.Slice(13 * sizeof(int)));
+            // 8 Unsigned Integers
+            key.CopyTo(_state.AsSpan(_constants.Length * sizeof(uint)));
 
-            tableIntegers.CopyTo(_originalState);
-            for (int i = 0; i < 10; i++)
+            // 1 Unsigned Integer
+            state[12] = blockCount;
+
+            // 3 Unsigned Integers
+            nonce.CopyTo(_state.AsSpan((nonce.Length == 8 ? 14 : 13) * sizeof(uint)));
+
+            if (blockCount > 0)
             {
-                QuarterRound(tableIntegers, 0, 4, 8, 12);
-                QuarterRound(tableIntegers, 1, 5, 9, 13);
-                QuarterRound(tableIntegers, 2, 6, 10, 14);
-                QuarterRound(tableIntegers, 3, 7, 11, 15);
-                QuarterRound(tableIntegers, 0, 5, 10, 15);
-                QuarterRound(tableIntegers, 1, 6, 11, 12);
-                QuarterRound(tableIntegers, 2, 7, 8, 13);
-                QuarterRound(tableIntegers, 3, 4, 9, 14);
+                for (int i = 0; i < blockCount; i++)
+                {
+                    RefreshBlock();
+                }
+                _position = 0; // Change from -1, as -1 will force the creation of a new block.
             }
         }
 
         public void Process(Span<byte> data) => Process(data, data);
         public void Process(ReadOnlySpan<byte> data, Span<byte> parsed)
         {
-            // TODO: https://tools.ietf.org/html/rfc7539#section-2.4.1
+            for (int i = 0; i < parsed.Length; i++)
+            {
+                if (_position == -1 || _position == 64)
+                {
+                    RefreshBlock();
+                    _position = 0;
+                }
+                parsed[i] = (byte)(data[i] ^ _block[_position++]);
+            }
+        }
+
+        private void RefreshBlock()
+        {
+            _block = new byte[64];
+            _state.CopyTo(_block, 0);
+
+            Span<uint> block = MemoryMarshal.Cast<byte, uint>(_block);
+            for (int i = 0; i < 10; i++)
+            {
+                QuarterRound(block, 0, 4, 8, 12);
+                QuarterRound(block, 1, 5, 9, 13);
+                QuarterRound(block, 2, 6, 10, 14);
+                QuarterRound(block, 3, 7, 11, 15);
+                QuarterRound(block, 0, 5, 10, 15);
+                QuarterRound(block, 1, 6, 11, 12);
+                QuarterRound(block, 2, 7, 8, 13);
+                QuarterRound(block, 3, 4, 9, 14);
+            }
+
+            Span<uint> state = MemoryMarshal.Cast<byte, uint>(_state);
+            for (int i = 0; i < 16; i++)
+            {
+                block[i] = CarrylessAdd(block[i], state[i]);
+            }
+            state[12]++;
         }
 
         private static void QuarterRound(Span<uint> state, int a, int b, int c, int d)
         {
-            state[d] = RotateLeft(state[d] ^ (state[a] += state[b]), 16);
-            state[b] = RotateLeft(state[b] ^ (state[c] += state[d]), 12);
-            state[d] = RotateLeft(state[d] ^ (state[a] += state[b]), 8);
-            state[b] = RotateLeft(state[b] ^ (state[c] += state[d]), 7);
+            state[d] = RotateLeft(CarrylessXor(state[d], (state[a] = CarrylessAdd(state[a], state[b]))), 16);
+            state[b] = RotateLeft(CarrylessXor(state[b], (state[c] = CarrylessAdd(state[c], state[d]))), 12);
+            state[d] = RotateLeft(CarrylessXor(state[d], (state[a] = CarrylessAdd(state[a], state[b]))), 8);
+            state[b] = RotateLeft(CarrylessXor(state[b], (state[c] = CarrylessAdd(state[c], state[d]))), 7);
+            //state[d] = RotateLeft(state[d] ^ (state[a] += state[b]), 16);
+            //state[b] = RotateLeft(state[b] ^ (state[c] += state[d]), 12);
+            //state[d] = RotateLeft(state[d] ^ (state[a] += state[b]), 8);
+            //state[b] = RotateLeft(state[b] ^ (state[c] += state[d]), 7);
         }
-        private static uint RotateLeft(uint value, int count) => (value << count) | (value >> (32 - count));
+
+        private static uint CarrylessXor(uint left, uint right) => unchecked(left ^ right);
+        private static uint CarrylessAdd(uint left, uint right) => unchecked(left + right);
+        private static uint RotateLeft(uint value, int count) => unchecked((value << count) | (value >> (32 - count)));
 
         public void Dispose()
         {
@@ -66,7 +111,7 @@ namespace Sulakore.Cryptography.Ciphers
             if (_disposed) return;
             if (disposing)
             {
-                _table = null;
+                _block = null;
             }
             _disposed = true;
         }

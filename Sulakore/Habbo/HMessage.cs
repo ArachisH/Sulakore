9f5c807c76a66154d88b1e659f9bef2ba4f70bcf
ArachisH <iKBenPeanut@gmail.com>
2020-12-31 14:34:51 -0800

    Utilize record keyword on this type

--- Sulakore/Habbo/HMessage.cs
+++ Sulakore/Habbo/HMessage.cs
@@ -1,28 +1,10 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 
 namespace Sulakore.Habbo
 {
     [DebuggerDisplay("{Id,nq}")]
-    public class HMessage : IEquatable<HMessage>
+    public record HMessage(ushort Id, string Name, bool IsOutgoing)
     {
-        public ushort Id { get; }
-        public string Name { get; }
-        public bool IsOutgoing { get; }
-
         public static implicit operator ushort(HMessage message) => message?.Id ?? ushort.MaxValue;
-
-        public HMessage(short id, string name, bool isOutgoing)
-        {
-            Id = (ushort)id; // TODO: Use short everywhere
-            Name = name;
-            IsOutgoing = isOutgoing;
-        }
-
-        public override string ToString() => Name;
-        public override int GetHashCode() => HashCode.Combine(IsOutgoing, Id);
-
-        public override bool Equals(object obj) => obj is HMessage message && Equals(message);
-        public bool Equals(HMessage other) => IsOutgoing == other.IsOutgoing && Id == other.Id;
     }
 }

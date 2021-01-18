fd8673383b9a84b73cc82ec08e4c4c72d7dc9c17
Rodrigo <ro4lan@gmail.com>
2021-01-18 15:48:32 -0300

    Create HRoomError.cs
    Added room entry error

---
+++ Sulakore/Habbo/Enums/HRoomError.cs
@@ -0,0 +1,11 @@
+namespace Sulakore.Habbo
+{
+    public enum HRoomError
+    {
+        RoomKickedByRoomController = 4008,
+        FlatEntryFailed = -100004,
+        NotFriendWithOwner = -100003,
+        WrongPasswordError = -100002,
+        PasswordRequiredError = -100001
+    }
+}

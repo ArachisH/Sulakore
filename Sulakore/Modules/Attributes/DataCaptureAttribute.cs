6857189e2df8e99ef2e07cf4a90e3eab40f6ff77
ArachisH <iKBenPeanut@gmail.com>
2022-03-12 15:24:47 -0800

    TService has been temporarily been 'disabled' Adjust some calls in based on the changes in HFormat

--- Sulakore/Modules/Attributes/DataCaptureAttribute.cs
+++ Sulakore/Modules/Attributes/DataCaptureAttribute.cs
@@ -2,97 +2,98 @@
 
 using Sulakore.Habbo;
 using Sulakore.Network;
+using Sulakore.Network.Buffers;
 
-namespace Sulakore.Modules
+namespace Sulakore.Modules;
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+public sealed class DataCaptureAttribute : Attribute, IEquatable<DataCaptureAttribute>
 {
-    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
-    public abstract class DataCaptureAttribute : Attribute, IEquatable<DataCaptureAttribute>
+    public short Id { get; init; }
+    public uint Hash { get; init; }
+    public bool IsOutgoing { get; init; }
+
+    internal object Target { get; set; }
+    internal MethodInfo Method { get; set; }
+
+    public static bool operator !=(DataCaptureAttribute left, DataCaptureAttribute right) => !(left == right);
+    public static bool operator ==(DataCaptureAttribute left, DataCaptureAttribute right) => EqualityComparer<DataCaptureAttribute>.Default.Equals(left, right);
+
+    public DataCaptureAttribute(short id, bool isOutgoing)
     {
-        public short Id { get; }
-        public uint Hash { get; }
-        public abstract bool IsOutgoing { get; }
+        Id = id;
+        IsOutgoing = isOutgoing;
+    }
+    public DataCaptureAttribute(uint hash, bool isOutgoing)
+    {
+        Hash = hash;
+        IsOutgoing = isOutgoing;
+    }
 
-        internal object Target { get; set; }
-        internal MethodInfo Method { get; set; }
-
-        public static bool operator !=(DataCaptureAttribute left, DataCaptureAttribute right) => !(left == right);
-        public static bool operator ==(DataCaptureAttribute left, DataCaptureAttribute right) => EqualityComparer<DataCaptureAttribute>.Default.Equals(left, right);
-
-        public DataCaptureAttribute(short id)
+    internal void Invoke(DataInterceptedEventArgs args)
+    {
+        object[] parameters = CreateValues(args);
+        object result = Method?.Invoke(Target, parameters);
+        switch (result)
         {
-            Id = id;
-        }
-        public DataCaptureAttribute(uint hash)
-        {
-            Hash = hash;
-        }
-
-        internal void Invoke(DataInterceptedEventArgs args)
-        {
-            object[] parameters = CreateValues(args);
-            object result = Method?.Invoke(Target, parameters);
-            switch (result)
+            case bool isBlocked:
             {
-                case bool isBlocked:
-                {
-                    args.IsBlocked = isBlocked;
-                    break;
-                }
+                args.IsBlocked = isBlocked;
+                break;
             }
         }
-        private object[] CreateValues(DataInterceptedEventArgs args)
-        {
-            ParameterInfo[] parameters = Method.GetParameters();
-            var values = new object[parameters.Length];
+    }
+    private object[] CreateValues(DataInterceptedEventArgs args)
+    {
+        ParameterInfo[] parameters = Method.GetParameters();
+        var values = new object[parameters.Length];
 
-            int position = 0;
-            var packet = new HReadOnlyPacket(args.Packet.Span, args.Format);
-            for (int i = 0; i < values.Length; i++)
+        HPacketReader packetIn = args.Packet.AsReader();
+        for (int i = 0; i < values.Length; i++)
+        {
+            ParameterInfo parameter = parameters[i];
+            if (parameter.Name.Equals("id", StringComparison.OrdinalIgnoreCase))
             {
-                ParameterInfo parameter = parameters[i];
-                if (parameter.Name.Equals("id", StringComparison.OrdinalIgnoreCase))
-                {
-                    values[i] = packet.Id;
-                }
-                values[i] = Type.GetTypeCode(parameter.ParameterType) switch
-                {
-                    TypeCode.Byte => packet[position++],
-                    TypeCode.Int32 => packet.ReadInt32(ref position),
-                    TypeCode.Int16 => packet.ReadInt16(ref position),
-                    TypeCode.String => packet.ReadUTF8(ref position),
-                    TypeCode.Single => packet.ReadFloat(ref position),
-                    TypeCode.Double => packet.ReadDouble(ref position),
-                    TypeCode.Boolean => packet.ReadBoolean(ref position),
-                    _ => CreateUnknownValueType(packet, ref position, args, parameter.ParameterType)
-                };
+                values[i] = args.Packet.Id;
             }
-            return values;
+            values[i] = Type.GetTypeCode(parameter.ParameterType) switch
+            {
+                TypeCode.Byte => packetIn[packetIn.Position++],
+                TypeCode.Int32 => packetIn.Read<int>(),
+                TypeCode.Int16 => packetIn.Read<short>(),
+                TypeCode.String => packetIn.ReadUTF8(),
+                TypeCode.Single => packetIn.Read<float>(),
+                TypeCode.Double => packetIn.Read<double>(),
+                TypeCode.Boolean => packetIn.Read<bool>(),
+                _ => CreateUnknownValueType(ref packetIn, args, parameter.ParameterType)
+            };
         }
-        private object CreateUnknownValueType(HReadOnlyPacket packet, ref int position, DataInterceptedEventArgs args, Type parameterType) => parameterType.Name switch
-        {
-            nameof(DataInterceptedEventArgs) => args,
-            nameof(ReadOnlyMemory<byte>) => args.Packet,
-            nameof(HPoint) => new HPoint(packet.ReadInt32(ref position), packet.ReadInt32(ref position)),
+        return values;
+    }
+    private object CreateUnknownValueType(ref HPacketReader packet, DataInterceptedEventArgs args, Type parameterType) => parameterType.Name switch
+    {
+        nameof(DataInterceptedEventArgs) => args,
+        nameof(ReadOnlyMemory<byte>) => args.Packet,
+        nameof(HPoint) => new HPoint(packet.Read<int>(), packet.Read<int>()),
 
-            _ => null
-        };
+        _ => null
+    };
 
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(Id, Identifier, IsOutgoing, Method);
-        }
-        public override bool Equals(object obj)
-        {
-            return Equals(obj as DataCaptureAttribute);
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Id, Hash, IsOutgoing, Method);
+    }
+    public override bool Equals(object obj)
+    {
+        return Equals(obj as DataCaptureAttribute);
 
-        }
-        public bool Equals(DataCaptureAttribute other)
-        {
-            return other != null &&
-                Id == other.Id &&
-                Identifier == other.Identifier &&
-                IsOutgoing == other.IsOutgoing &&
-                EqualityComparer<MethodInfo>.Default.Equals(Method, other.Method);
-        }
+    }
+    public bool Equals(DataCaptureAttribute other)
+    {
+        return other != null &&
+            Id == other.Id &&
+            Hash == other.Hash &&
+            IsOutgoing == other.IsOutgoing &&
+            EqualityComparer<MethodInfo>.Default.Equals(Method, other.Method);
     }
 }

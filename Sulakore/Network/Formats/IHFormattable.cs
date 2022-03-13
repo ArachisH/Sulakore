8e46271b64edeaf2e916412d8f07d1ebaa92120a
PaulusParssinen <paulus.parssinen@gmail.com>
2022-03-13 21:04:51 +0200

    Add IHFormattable

---
+++ Sulakore/Network/Formats/IHFormattable.cs
@@ -0,0 +1,9 @@
+﻿namespace Sulakore.Network.Formats;
+
+/// <summary>
+/// Provides functionality to format the binary representation of an object into 
+/// </summary>
+public interface IHFormattable
+{
+    bool TryFormat(Span<byte> destination, IHFormat format, out int bytesWritten, ReadOnlySpan<char> formatString);
+}

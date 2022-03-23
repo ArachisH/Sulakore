30a0a78c4f852695ac06331928e0eb3baa34562d
ArachisH <iKBenPeanut@gmail.com>
2022-03-22 21:38:14 -0700

    Derived types should pass along the CancellationToken if possible

--- Sulakore/Network/IHConnection.cs
+++ Sulakore/Network/IHConnection.cs
@@ -11,6 +11,6 @@ public interface IHConnection
     IHFormat SendFormat { get; }
     IHFormat ReceiveFormat { get; }
 
-    Task SendToClientAsync(HPacket packet);
-    Task SendToServerAsync(HPacket packet);
+    Task SendToClientAsync(HPacket packet, CancellationToken cancellationToken = default);
+    Task SendToServerAsync(HPacket packet, CancellationToken cancellationToken = default);
 }

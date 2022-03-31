aefc46e14dec2ee3a11a0a80a4ba5465606e1aeb
ArachisH <iKBenPeanut@gmail.com>
2022-03-30 17:56:49 -0700

    Removed Modules namespace from the library Will be moved to the Tangine sub-project in Tanji

--- Sulakore/Modules/Attributes/OutMessageAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sulakore.Modules
-{
-    public sealed class OutMessageAttribute : MessageAttribute
-    {
-        public OutMessageAttribute(string identifier)
-            : base(identifier, true)
-        { }
-    }
-}

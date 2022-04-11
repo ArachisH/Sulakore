9ea9a1ae9fc6c0f55bfd335fc13795acbf5fe818
ArachisH <iKBenPeanut@gmail.com>
2022-04-11 13:54:33 -0700

    Moved the IsExternalInit class into its own file

--- Sulakore.Generators/MessagesGenerator.cs
+++ Sulakore.Generators/MessagesGenerator.cs
@@ -7,98 +7,92 @@ using System.Runtime.Serialization.Json;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Text;
 
-namespace Sulakore.Generators
+namespace Sulakore.Generators;
+
+[Generator]
+public sealed class MessagesGenerator : ISourceGenerator
 {
-    [Generator]
-    public class MessagesGenerator : ISourceGenerator
+    public void Execute(GeneratorExecutionContext context)
     {
-        public void Execute(GeneratorExecutionContext context)
-        {
-            string messagesJsonPath = context.AdditionalFiles.Single(at => at.Path.EndsWith("messages.json")).Path;
-            using var messagesStream = new MemoryStream(File.ReadAllBytes(messagesJsonPath));
+        string messagesJsonPath = context.AdditionalFiles.Single(at => at.Path.EndsWith("messages.json")).Path;
+        using var messagesStream = new MemoryStream(File.ReadAllBytes(messagesJsonPath));
 
-            var messagesSerializer = new DataContractJsonSerializer(typeof(Messages));
-            var messages = (Messages)messagesSerializer.ReadObject(messagesStream);
+        var messagesSerializer = new DataContractJsonSerializer(typeof(Messages));
+        var messages = (Messages)messagesSerializer.ReadObject(messagesStream);
 
-            context.AddSource("Outgoing.cs", SourceText.From(CreateMessagesSource(messages.Outgoing, true), Encoding.UTF8));
-            context.AddSource("Incoming.cs", SourceText.From(CreateMessagesSource(messages.Incoming, false), Encoding.UTF8));
-        }
-        public void Initialize(GeneratorInitializationContext context)
-        { }
-
-        private static string CreateMessagesSource(Message[] messages, bool isOutgoing)
-        {
-            string className = isOutgoing ? "Outgoing" : "Incoming";
-            string isOutgoingString = isOutgoing.ToString().ToLowerInvariant();
-
-            using var text = new StringWriter();
-            using var indentedText = new IndentedTextWriter(text);
-
-            indentedText.WriteLine("namespace Sulakore.Habbo");
-            indentedText.WriteLine('{');
-            indentedText.Indent++;
-
-            indentedText.WriteLine($"public sealed class {className} : Identifiers");
-            indentedText.WriteLine('{');
-            indentedText.Indent++;
-
-            indentedText.Write("#region Message Properties");
-            foreach (Message message in messages)
-            {
-                indentedText.WriteLine();
-                message.BackingFieldName = $"_{char.ToLower(message.Name[0]) + message.Name.Substring(1)}";
-                indentedText.WriteLine($"private HMessage {message.BackingFieldName};");
-                indentedText.WriteLine($"public HMessage {message.Name}");
-                indentedText.WriteLine('{');
-
-                indentedText.Indent++;
-                indentedText.WriteLine($"get => {message.BackingFieldName};");
-                indentedText.WriteLine($"init => Register(value, \"{message.Name}\", ref {message.BackingFieldName});");
-                indentedText.Indent--;
-
-                indentedText.WriteLine('}');
-            }
-            indentedText.WriteLine("#endregion");
-
-            indentedText.WriteLine();
-            indentedText.WriteLine($"public {className}() : base({isOutgoingString}) {{ }}");
-            indentedText.WriteLine($"public {className}(IGame game) : base({messages.Length}, {isOutgoingString})");
-            indentedText.WriteLine('{');
-            indentedText.Indent++;
-
-            foreach (Message message in messages)
-            {
-                indentedText.Write($"{message.BackingFieldName} = ResolveMessage(game, \"{message.Name}\", {message.UnityId}, ");
-                if (string.IsNullOrWhiteSpace(message.UnityStructure))
-                {
-                    indentedText.Write("null");
-                }
-                else indentedText.Write("\"" + message.UnityStructure + "\"");
-
-                foreach (uint postShuffleHash in message.PostShuffleHashes)
-                {
-                    indentedText.Write(", ");
-                    indentedText.Write(postShuffleHash);
-                }
-                indentedText.WriteLine(");");
-            }
-
-            indentedText.Indent--;
-            indentedText.WriteLine('}');
-
-            indentedText.Indent--;
-            indentedText.WriteLine('}');
-
-            indentedText.Indent--;
-            indentedText.WriteLine('}');
-
-            indentedText.Flush();
-            return text.ToString();
-        }
+        context.AddSource("Outgoing.cs", SourceText.From(CreateMessagesSource(messages.Outgoing, true), Encoding.UTF8));
+        context.AddSource("Incoming.cs", SourceText.From(CreateMessagesSource(messages.Incoming, false), Encoding.UTF8));
     }
-}
+    public void Initialize(GeneratorInitializationContext context)
+    { }
 
-namespace System.Runtime.CompilerServices
-{
-    public class IsExternalInit { }
+    private static string CreateMessagesSource(Message[] messages, bool isOutgoing)
+    {
+        string className = isOutgoing ? "Outgoing" : "Incoming";
+        string isOutgoingString = isOutgoing.ToString().ToLowerInvariant();
+
+        using var text = new StringWriter();
+        using var indentedText = new IndentedTextWriter(text);
+
+        indentedText.WriteLine("namespace Sulakore.Habbo");
+        indentedText.WriteLine('{');
+        indentedText.Indent++;
+
+        indentedText.WriteLine($"public sealed class {className} : Identifiers");
+        indentedText.WriteLine('{');
+        indentedText.Indent++;
+
+        indentedText.Write("#region Message Properties");
+        foreach (Message message in messages)
+        {
+            indentedText.WriteLine();
+            message.BackingFieldName = $"_{char.ToLower(message.Name[0]) + message.Name.Substring(1)}";
+            indentedText.WriteLine($"private HMessage {message.BackingFieldName};");
+            indentedText.WriteLine($"public HMessage {message.Name}");
+            indentedText.WriteLine('{');
+
+            indentedText.Indent++;
+            indentedText.WriteLine($"get => {message.BackingFieldName};");
+            indentedText.WriteLine($"init => Register(value, \"{message.Name}\", ref {message.BackingFieldName});");
+            indentedText.Indent--;
+
+            indentedText.WriteLine('}');
+        }
+        indentedText.WriteLine("#endregion");
+
+        indentedText.WriteLine();
+        indentedText.WriteLine($"public {className}() : base({isOutgoingString}) {{ }}");
+        indentedText.WriteLine($"public {className}(IGame game) : base({messages.Length}, {isOutgoingString})");
+        indentedText.WriteLine('{');
+        indentedText.Indent++;
+
+        foreach (Message message in messages)
+        {
+            indentedText.Write($"{message.BackingFieldName} = ResolveMessage(game, \"{message.Name}\", {message.UnityId}, ");
+            if (string.IsNullOrWhiteSpace(message.UnityStructure))
+            {
+                indentedText.Write("null");
+            }
+            else indentedText.Write("\"" + message.UnityStructure + "\"");
+
+            foreach (uint postShuffleHash in message.PostShuffleHashes)
+            {
+                indentedText.Write(", ");
+                indentedText.Write(postShuffleHash);
+            }
+            indentedText.WriteLine(");");
+        }
+
+        indentedText.Indent--;
+        indentedText.WriteLine('}');
+
+        indentedText.Indent--;
+        indentedText.WriteLine('}');
+
+        indentedText.Indent--;
+        indentedText.WriteLine('}');
+
+        indentedText.Flush();
+        return text.ToString();
+    }
 }

69edda542fb00b186d98b336655cbc887054f355
ArachisH <iKBenPeanut@gmail.com>
2022-04-14 20:10:51 -0700

    Implementations of IGame should have knowledge on the send/receive packet format Mark the Path, and Revision properties as nullable, since the implementation may not have this information

--- Sulakore/Habbo/IGame.cs
+++ Sulakore/Habbo/IGame.cs
@@ -1,12 +1,17 @@
-﻿namespace Sulakore.Habbo;
+﻿using Sulakore.Network.Formats;
+
+namespace Sulakore.Habbo;
 
 public interface IGame
 {
     bool IsPostShuffle { get; }
     HPlatform Platform { get; }
 
-    string Path { get; }
-    string Revision { get; }
+    IHFormat ClientSendPacketFormat { get; }
+    IHFormat ClientReceivePacketFormat { get; }
+
+    string? Path { get; }
+    string? Revision { get; }
     int MinimumConnectionAttempts { get; }
 
     bool TryResolveMessage(string name, uint hash, bool isOutgoing, out HMessage message);

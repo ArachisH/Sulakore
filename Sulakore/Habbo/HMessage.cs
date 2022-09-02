75098b9e4a263f117402677c944e7b0f825066a6
PaulusParssinen <paulus.parssinen@gmail.com>
2022-09-02 03:25:46 +0300

    Make message definition generator incremental

--- Sulakore/Habbo/HMessage.cs
+++ Sulakore/Habbo/HMessage.cs
@@ -3,7 +3,7 @@
 namespace Sulakore.Habbo;
 
 [DebuggerDisplay("{Id,nq}")]
-public readonly record struct HMessage(string? Name, short Id, uint Hash, string? Structure, bool IsOutgoing, string? TypeName, string? ParserTypeName, int References)
+public record HMessage(string? Name, short Id, uint Hash, string? Structure, bool IsOutgoing, string? TypeName, string? ParserTypeName, int References)
 {
     public static implicit operator short(HMessage message) => message.Id;
     public static implicit operator string?(HMessage message) => message.Name;

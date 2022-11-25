984d7f93f460f72aaeeb15826809db1500ebf6b5
ArachisH <iKBenPeanut@gmail.com>
2022-11-25 13:56:03 -0800

    Renamed ServerEndPoint property to RemoteEndPoint

--- Sulakore/Network/IHConnection.cs
+++ Sulakore/Network/IHConnection.cs
@@ -6,7 +6,7 @@ public interface IHConnection
 {
     Incoming? In { get; }
     Outgoing? Out { get; }
-    HotelEndPoint ServerEndPoint { get; }
+    HotelEndPoint RemoteEndPoint { get; }
 
     ValueTask SendToClientAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = default);
     ValueTask SendToServerAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = default);
